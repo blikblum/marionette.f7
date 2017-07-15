a8ecec7072d8e87c1f0c66c172bf91e7d27b2c4c
Luiz Américo <brandkarl@gmail.com>
2017-07-15 19:39:21 -0300

    Implement showView, rootPage and preRender features

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import _ from 'underscore'
+
 const f7ViewMap = Object.create(null)
 let f7App
 
-function getF7View (target) {
-  if (!target) {
-    target = f7App.getCurrentView(0).params.name
+function getF7ViewConfig (name) {
+  if (!name) {
+    name = f7App.getCurrentView(0).params.name
   }
-  return f7ViewMap[target]
+  return f7ViewMap[name]
 }
 
 function getMnViewFromEvent (event) {
@@ -29,31 +31,69 @@ Dom7(document).on('page:beforeremove', function (e) {
 })
 
 export function configureApp ({app, views = {}}) {
+  let toRender = []
   f7App = app
-  views.main = '.view-main'
+  if (!views.main) views.main = '.view-main'
   Object.keys(views).forEach((viewName) => {
-    f7ViewMap[viewName] = app.addView(views[viewName], {
+    let options = views[viewName]
+    if (typeof options === 'string') options = {el: options}
+    let f7View = app.addView(options.el, {
       name: viewName,
+      dynamicNavbar: true,
       domCache: true
     })
+    f7ViewMap[viewName] = {
+      instance: f7View,
+      options: options
+    }
+    if (options.preRender && options.rootPage) {
+      toRender.push(_.result(options, 'rootPage'))
+    }
   })
+  if (toRender.length) {
+    Promise.all(toRender).then(function (views) {
+      views.forEach(function (view) {
+        view.render()
+      })
+    })
+  }
 }
 
-export function pushPage (view, target) {
-  let f7View = getF7View(target)
+export function pushPage (view, viewName, options) {
+  let f7View = getF7ViewConfig(viewName).instance
   if (!view.isRendered()) {
     view.render()
   }
   view.triggerMethod('before:attach', view)
-  f7View.router.load({
+  f7View.router.load(Object.assign({}, options, {
     pageElement: view.el,
-    context: {
-      __mn_view__: view
-    }
+    context: {__mn_view__: view}
   })
+  )
 }
 
-export function popPage (target) {
-  let f7View = getF7View(target)
+export function popPage (viewName) {
+  let f7View = getF7ViewConfig(viewName).instance
   f7View.router.back()
+}
+
+function updateActiveView (view) {
+  let $views = Dom7(view.container).parent('.views')
+  $views.children('.view.active').removeClass('active')
+  view.container.classList.add('active')
+}
+
+export function showView (viewName) {
+  let viewConfig = getF7ViewConfig(viewName)
+  let view = viewConfig.instance
+  if (!view.pagesContainer.children.length && viewConfig.options.rootPage) {
+    let rootPage = _.result(viewConfig.options, 'rootPage')
+    return Promise.resolve(rootPage).then(function (pageView) {
+      pushPage(pageView, viewName, {animatePages: false})
+      updateActiveView(view)
+    })
+  } else {
+    updateActiveView(view)
+    return Promise.resolve()
+  }
 }
