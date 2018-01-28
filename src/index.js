ae7ff6abe51cc3e7e251ed90e1d09152dec7ecec
Luiz Américo <luizamericop@gmail.com>
2018-01-28 11:35:10 -0300

    Update implementation to use f7 v2 api

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import _ from 'underscore'
+import Framework7, {Dom7} from 'framework7'
 
 const f7ViewMap = Object.create(null)
 let f7App
 
 function getF7ViewConfig (name) {
   if (!name) {
-    name = f7App.getCurrentView(0).params.name
+    name = f7App.views.current.params.name
   }
   return f7ViewMap[name]
 }
 
+function getF7ViewPages (view) {
+  return view.$el.find('.page')
+}
+
 function getMnViewFromEvent (event) {
-  let context = event.detail.page.context
-  return context && context.__mn_view__
+  let f7Route = event.detail.route
+  return f7Route && f7Route.route && f7Route.route.__mn_view__
 }
 
 Dom7(document).on('page:beforeinit', function (e) {
-  let view = getMnViewFromEvent(e)
+  const view = getMnViewFromEvent(e)
   if (view) {
     view._isAttached = true
     view.triggerMethod('attach', view)
@@ -49,10 +54,9 @@ export function createApp ({options = {}, views = {}}) {
   Object.keys(views).forEach((viewName) => {
     let viewOptions = views[viewName]
     if (typeof viewOptions === 'string') viewOptions = {el: viewOptions}
-    let f7View = f7App.addView(viewOptions.el, {
+    let f7View = f7App.views.create(viewOptions.el, {
       name: viewName,
-      dynamicNavbar: true,
-      domCache: true
+      stackPages: true
     })
     f7ViewMap[viewName] = {
       instance: f7View,
@@ -75,10 +79,10 @@ export function createApp ({options = {}, views = {}}) {
 }
 
 function destroyPages (view) {
-  let $pages = Dom7(view.pagesContainer).children('.page')
+  const $pages = Dom7(view.el).children('.page')
   $pages.each(function () {
-    let data = this.f7PageData
-    let mnView = data && data.context && data.context.__mn_view__
+    const page = this.f7Page
+    const mnView = page && page.route && page.route.route && page.route.route.__mn_view__
     if (mnView) mnView.destroy()
   })
 }
@@ -86,10 +90,10 @@ function destroyPages (view) {
 export function pushPage (view, viewName, options = {}) {
   let f7ViewConfig = getF7ViewConfig(viewName)
   let f7View = f7ViewConfig.instance
-  let loadPopup = f7View.pagesContainer.children.length === 0 && f7ViewConfig.options.popup
+  let loadPopup = f7ViewConfig.options.popup && !getF7ViewPages(f7View).length
   let containerHTML
   if (loadPopup) {
-    containerHTML = f7View.container.innerHTML
+    containerHTML = f7View.el.innerHTML
     options.animatePages = false
   }
   if (!view.isRendered()) {
@@ -97,23 +101,20 @@ export function pushPage (view, viewName, options = {}) {
   }
   view.triggerMethod('before:attach', view)
   view.el.dataset.page = _.uniqueId('mn-page-')
-  f7View.router.load(Object.assign({}, options, {
-    pageElement: view.el,
-    context: {__mn_view__: view}
-  })
+  f7View.router.load(Object.assign({}, options, {el: view.el}),
+    {route: {route: {__mn_view__: view}}}
   )
   if (loadPopup) {
-    let $popup = Dom7(f7View.container).closest('.popup')
+    let $popup = Dom7(f7View.el).closest('.popup')
     $popup.once('popup:closed', function () {
       destroyPages(f7View)
       // manually reset the view using brute force
       let viewName = f7View.params.name
-      f7View.container.innerHTML = containerHTML
+      f7View.el.innerHTML = containerHTML
       f7View.destroy()
-      f7ViewConfig.instance = f7App.addView(f7ViewConfig.options.el, {
+      f7ViewConfig.instance = f7App.views.create(f7ViewConfig.options.el, {
         name: viewName,
-        dynamicNavbar: true,
-        domCache: true
+        stackPages: true
       })
     })
     f7App.popup($popup)
@@ -123,11 +124,6 @@ export function pushPage (view, viewName, options = {}) {
 export function popPage (viewName) {
   let f7View = getF7ViewConfig(viewName).instance
   f7View.router.back()
-}
-
-function popupCloseClick (e) {
-  e.preventDefault()
-  f7App.closeModal(this.el)
 }
 
 export function showPopup (view) {
@@ -141,15 +137,16 @@ export function showPopup (view) {
 }
 
 function updateActiveView (view) {
-  let $views = Dom7(view.container).parent('.views')
-  $views.children('.view.active').removeClass('active')
-  view.container.classList.add('active')
+  const $views = Dom7(view.el).parent('.views')
+  if (!$views.length) return
+  $views.children('.view.tab-active').removeClass('tab-active')
+  view.el.classList.add('tab-active')
 }
 
 export function showView (viewName) {
   let viewConfig = getF7ViewConfig(viewName)
   let view = viewConfig.instance
-  if (!view.pagesContainer.children.length && viewConfig.options.rootPage) {
+  if (viewConfig.options.rootPage && !getF7ViewPages(view).length) {
     let rootPage = _.result(viewConfig.options, 'rootPage')
     return Promise.resolve(rootPage).then(function (pageView) {
       pushPage(pageView, viewName, {animatePages: false})
